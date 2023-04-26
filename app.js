00e1391ea4543bb09d230806d45d8dcfbaf88c81
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-04-26 15:21:13 +0200

    keyframes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const img1 = document.querySelector('.container-images img:nth-child(1)')
 const btnCat = document.querySelector('.cat-fade')
 // gsap.to(img1, {x: 100, duration: 2})
-gsap.to('.container-images img', {y: 300, scale: 1.2, duration: 2, ease: 'power1', stagger: function(index){
-    return index * 0.2
-}
-})
+gsap.to('.container-images img:nth-child(2)', {keyframes: [
+    {duration: 0.3, y: 100},
+    {duration: 0.3, scale: 1.3},
+    {duration: 0.3, y: 200},
+]})
 
 // gsap.from(img1, {x: 100, duration: 2})
 
