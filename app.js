ea214a8c3b7b8ebf1fdc15afcf90121efcfff51a
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-04-26 15:53:03 +0200

    register effect

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const img1 = document.querySelector('.container-images img:nth-child(1)')
 const btnCat = document.querySelector('.cat-fade')
 // gsap.to(img1, {x: 100, duration: 2})
-gsap.to('.container-images img:nth-child(2)', {keyframes: [
-    {duration: 0.3, y: 100},
-    {duration: 0.3, scale: 1.3},
-    {duration: 0.3, y: 200},
-]})
+gsap.to('.container-images img:nth-child(2)', {
+    y: 100,
+    repeat: 1,
+    onComplete: () => console.log('terminée'),
+    onStart: () => console.log('démarrée'),
+    onUpdate: () => console.log('en cours'),
+    onRepeat: () => console.log('répétée'),
+})
 
 // gsap.from(img1, {x: 100, duration: 2})
 
@@ -16,3 +19,14 @@ gsap.to('.container-images img:nth-child(2)', {keyframes: [
 // })
 
 gsap.to('.container-btns button', {duration: 0.2, x: "random(-100, 100)", stagger: 0.2})
+
+// Enregistrer un tween
+gsap.registerEffect({
+    name: "imgCrazy",
+    effect: (targets, config) => {
+        return gsap.to(targets, {duration: config.duration, y: 200, scale: 1.4, rotation: 360})
+    },
+    defaults: {duration: 2}
+})
+
+gsap.effects.imgCrazy(img1, {duration: 1})
