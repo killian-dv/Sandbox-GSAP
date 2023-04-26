5d10c7f37b2f79e7e32a3f01141af0fd636794c3
killian-dv <killian.david@etudiant.univ-reims.fr>
2023-04-26 16:29:18 +0200

    timeline

--- app.js
+++ app.js
@@ -1,40 +1,15 @@
 const img1 = document.querySelector('.container-images img:nth-child(1)')
-const btnCat = document.querySelector('.cat-fade')
-// gsap.to(img1, {x: 100, duration: 2})
-gsap.to('.container-images img:nth-child(2)', {
-    y: 100,
-    repeat: 1,
-    onComplete: () => console.log('terminée'),
-    onStart: () => console.log('démarrée'),
-    onUpdate: () => console.log('en cours'),
-    onRepeat: () => console.log('répétée'),
-})
+const img2 = document.querySelector('.container-images img:nth-child(2)')
+const img3 = document.querySelector('.container-images img:nth-child(3)')
+const title = document.querySelector('.title')
+const txt = document.querySelector('.txt')
 
-// gsap.from(img1, {x: 100, duration: 2})
+// Instancier une timeline
+const TL = gsap.timeline()
 
-// btnCat.addEventListener('click', () => {
-//     // gsap.from(img1, {opacity: 0, duration: 3})
-//     // gsap.fromTo(img1, {opacity: 0}, {opacity: 1, duration: 3})
-//     // gsap.to(img1, {opacity: 0, scale: 2, duration: 3, delay: 2})
-// })
-
-gsap.to('.container-btns button', {duration: 0.2, x: "random(-100, 100)", stagger: 0.2})
-
-// Enregistrer un tween
-// gsap.registerEffect({
-//     name: "imgCrazy",
-//     effect: (targets, config) => {
-//         return gsap.to(targets, {duration: config.duration, y: 200, scale: 1.4, rotation: 360})
-//     },
-//     defaults: {duration: 2}
-// })
-
-// gsap.effects.imgCrazy(img1, {duration: 1})
-
-// gsap.set('.container-images img', {opacity: 0})
-
-const anim = gsap.to(img1, {y: 100, yoyo: true, repeat: 1})
-anim.duration(5)
-anim.pause()
-anim.seek(2.5)
-anim.resume()
+TL
+.to(img1, {autoAlpha: 1, y: 0})
+.to(img2, {autoAlpha: 1, y: 0})
+.to(img3, {autoAlpha: 1, y: 0})
+.to(title, {autoAlpha: 1, y: 0})
+.to(txt, {autoAlpha: 1, y: 0})
